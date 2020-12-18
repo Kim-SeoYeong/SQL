e2044daeeb1ca1d4a756ccae788731396aeb584b
kimsy <duddl2102@naver.com>
2020-12-18 17:03:49 +0900

    2020/12/18 그룹함수, case-end문 과제 커밋

--- Practice/practice02.sql
+++ Practice/practice02.sql
@@ -15,17 +15,20 @@ where manager_id is not null;
 -- 직원중에 최고임금(salary)과  최저임금을 “최고임금, “최저임금”프로젝션 타이틀로 함께 출력해 보세요. 
 -- 두 임금의 차이는 얼마인가요?  “최고임금 – 최저임금”이란 타이틀로 함께 출력해 보세요.
 
-select  max(salary) "최고임금",
+select  job_id "직원",
+        max(salary) "최고임금",
         min(salary) "최저임금",
         (max(salary) - min(salary)) "최고임금 - 최저임금"
-from employees;
+from employees
+group by job_id
+order by "최고임금 - 최저임금" asc;
 
 
 -- 문제3.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
 
-select to_char(max(hire_date), 'YYYY"년" MM"월" DD"일"')
+select to_char(max(hire_date), 'YYYY"년" MM"월" DD"일"') "입사일"
 from employees;
 
 
@@ -122,3 +125,6 @@ select  first_name "이름",
         end as optDate
 from employees
 order by hire_date asc;
+
+
+
